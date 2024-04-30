f0cfe141f0cba7acd62488f2e41911e04b06bc4d
asuasuasuasuasuasu <a.nakamur@so-ra.co.jp>
2024-04-30 17:34:53 +0900

    一旦完成

--- delete.php
+++ delete.php
@@ -1,5 +1,4 @@
 <?php
-
   $user = "root";
   $password = "root";
   $dbh = new PDO("mysql:host=localhost; dbname=bbs; charset=utf8", "$user", "$password");
@@ -12,34 +11,21 @@
     //レコード取得
   $row = $stmt->fetch();
   
-
   $stmt = $dbh->prepare("DELETE FROM comments WHERE id = :id");
   $stmt->bindParam(':id', $id, PDO::PARAM_INT);
   $stmt->execute();
   echo "削除しました。";
-
-  // $stmt = $dbh->prepare('DELETE FROM comments WHERE id = :id');
-  // $stmt->execute(array(':id' => $_GET["id"]));
-  // echo "削除しました。";
-
-
-
-  // $stmt = $db->prepare("SELECT id,body FROM comments WHERE message_id = :postId");
-  // $stmt->bindParam(':postId', $postId, PDO::PARAM_INT);
-  // $stmt->execute();
-
-
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>削除完了</title>
-  </head>
-  <body>          
+<head>
+  <meta charset="utf-8">
+  <title>削除完了</title>
+</head>
+<body>          
   <p>
       <a href="show.php?id=<?php echo $row['message_id']; ?>">投稿詳細へ</a>
   </p> 
-  </body>
+</body>
 </html>
